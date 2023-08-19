0d3c821627a04a9a4e09ef48bb32bf02ef719f07
imvishal28 <vishalmshindhe1997@gmail.com>
2023-08-19 18:56:58 +0530

    ASSIGNMENT PROGRAMS
    do while assignment programs

---
+++ AssignmentDoWhile-1/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=0;
+		vishal :do
+		{
+			int n=0;
+			vish : do
+			{
+				System.out.println("Hello Vishal");
+				n++;
+				break vishal ;
+			}while (n<5);
+				
+		}while (m<6);
+
+	}
+
+}
